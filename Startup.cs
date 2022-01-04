023be63c439ecbf0671bb89756260fe6225c1850
Thanit Konmek <thanit@frappet.com>
2022-01-05 00:45:54 +0700

    อัพเดรต cors wwwroot

--- Startup.cs
+++ Startup.cs
@@ -103,7 +103,16 @@ namespace backend
 
             app.UseAuthorization();
 
-            app.UseStaticFiles();
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                OnPrepareResponse = ctx =>
+                {
+                    ctx.Context.Response.Headers.Append("Access-Control-Allow-Origin", "*");
+                    ctx.Context.Response.Headers.Append("Access-Control-Allow-Headers",
+                       "Origin, X-Requested-With, Content-Type, Accept");
+                },
+
+            });
 
             app.UseEndpoints(endpoints =>
             {
